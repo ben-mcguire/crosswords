70fd69236e5b98a45ae276e1c54fa01ed16e2991
ben-mcguire <30700327+ben-mcguire@users.noreply.github.com>
2025-03-28 16:13:21 -0400

    Create string_in_a_string.r

---
+++ R_Scripts/string_in_a_string.r
@@ -0,0 +1,55 @@
+
+word_list <- read.csv("~/Desktop/xwordlist.csv", sep="")
+
+library(tidyverse)
+library(dplyr)
+
+words <- word_list$word
+
+# Define a target substring to remove
+target_string <- "EAST"
+
+# Define a position constraint: 'beginning', 'middle', or 'end'
+position <- "middle"
+
+# Define a function that looks for words that meet the following criteria:
+  # if you add your substring at the defined position to a string, you can that new string somewhere else in the list
+
+find_valid_words <- function(words, target_string, position) {
+  results <- data.frame(
+    Original_Word = character(),
+    Word_Removed  = character(),
+    Length        = integer(),
+    stringsAsFactors = FALSE
+  )
+  
+  for (word in words) {
+    if (grepl(target_string, word, fixed = TRUE)) {
+      modified_word <- gsub(target_string, "", word, fixed = TRUE)
+      
+      if (position == "beginning" && !grepl(paste0("^", target_string), word)) {
+        next
+      }
+      if (position == "middle" && (!grepl(paste0(".+", target_string, ".+"), word))) {
+        next
+      }
+      if (position == "end" && !grepl(paste0(target_string, "$"), word)) {
+        next
+      }
+      
+      if (modified_word %in% words) {
+        results <- rbind(results, data.frame(
+          Original_Word = word,
+          Word_Removed  = modified_word,
+          Length        = nchar(word),
+          stringsAsFactors = FALSE
+        ))
+      }
+    }
+  }
+  
+  return(results)
+}
+
+valid_words <- find_valid_words(words, target_string, position)
+View(valid_words)
